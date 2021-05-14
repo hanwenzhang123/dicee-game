7d9fdf43ea849af0173775c930e58cc453feffbc
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-05-13 22:39:41 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 
-var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6, decimal number
 
 var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
 
 var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
 
-var image1 = document.querySelectorAll("img")[0];
+var image1 = document.querySelectorAll("img")[0];   // the first image
 
-image1.setAttribute("src", randomImageSource);
+image1.setAttribute("src", randomImageSource);    //here the first one attribue name you want to change, second one is what you want to change to
 
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
